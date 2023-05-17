097dc665336773b262d782a5600a041afc80063a
BooyahSquadians69 <jiveshkalra69@gmail.com>
2023-05-17 16:43:05 +0530

    code optimisation

--- ai_models.py
+++ ai_models.py
@@ -1,14 +1,65 @@
-
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM,pipeline, AutoModelForQuestionAnswering
+import os
+import json
 
-def summarizer_model(query,max_length=1000):
-    tokenizer = AutoTokenizer.from_pretrained("pszemraj/pegasus-x-large-book-summary")
-    model_1 = AutoModelForSeq2SeqLM.from_pretrained("pszemraj/pegasus-x-large-book-summary")
-    input_ids = tokenizer.encode(query, truncation=True, padding=True, return_tensors="pt")
-    summary_ids = model_1.generate(input_ids, max_length=max_length, num_beams=4, early_stopping=True)
-    summary_1 = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    model_2 = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
-    summary_2 = model_2(query)[0]['summary_text']
-    return [summary_1,summary_2]
+class AI_models():
+    def summarizer(self,query,max_length=1000):
+        tokenizer = AutoTokenizer.from_pretrained("pszemraj/pegasus-x-large-book-summary")
+        model_1 = AutoModelForSeq2SeqLM.from_pretrained("pszemraj/pegasus-x-large-book-summary")
+        input_ids = tokenizer.encode(query, truncation=True, padding=True, return_tensors="pt")
+        summary_ids = model_1.generate(input_ids, max_length=max_length, num_beams=4, early_stopping=True)
+        summary_1 = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        model_2 = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
+        summary_2 = model_2(query)[0]['summary_text']
+        return [summary_1,summary_2]
+    
+    def question_analyzer(self,subject):
+        data_files = ['eng']
+        if subject in data_files:
+            json_path = os.path.join('static', 'json', f'{subject}.json')
+            with open(json_path) as file:
+                file_contents = file.read()
+                parsed_json = json.loads(file_contents)
+                return parsed_json
+        else:
+            return None
 
+    def question_answering(self,question,context):
+        qa_model_name = "deepset/roberta-base-squad2"
+        # a) Load model & tokenizer
+        model = AutoModelForQuestionAnswering.from_pretrained(qa_model_name)
+        tokenizer = AutoTokenizer.from_pretrained(qa_model_name)
+        nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+        QA_input = {'question':question,'context':context}
+        return nlp(QA_input)['answer']
+    
+    def paraphrase(
+        self,
+        question,
+        num_beams=5,
+        num_beam_groups=5,
+        num_return_sequences=5,
+        repetition_penalty=10.0,
+        diversity_penalty=3.0,
+        no_repeat_ngram_size=2,
+        temperature=0.7,
+        max_length=1000
+    ):
+        paraphrase_tokenizer = AutoTokenizer.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base")
+        paraphrase_model = AutoModelForSeq2SeqLM.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base").to(device)
+        input_ids = paraphrase_tokenizer(
+            f'paraphrase: {question}',
+            return_tensors="pt", padding="longest",
+            max_length=max_length,
+            truncation=True,
+        ).input_ids
+            
+        outputs = paraphrase_model.generate(
+            input_ids.to('cpu'), temperature=temperature, repetition_penalty=repetition_penalty,
+            num_return_sequences=num_return_sequences, no_repeat_ngram_size=no_repeat_ngram_size,
+            num_beams=num_beams, num_beam_groups=num_beam_groups,
+            max_length=max_length, diversity_penalty=diversity_penalty
+        )
 
+        res = paraphrase_tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        return res
--- server.py
+++ server.py
@@ -1,100 +1,16 @@
-from flask import Flask, render_template, request,url_for
+from flask import Flask, render_template, request
+from ai_models import AI_models
 from deep_translator import GoogleTranslator
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM,pipeline, AutoModelForQuestionAnswering
-from ai_models import summarizer_model
-import openai
-import os
-import json
 
 device = "cpu"
-openai.api_key = "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"
-
-
+AI_models = AI_models()
 app = Flask(__name__)
-
-def question_analyzer_model(subject):
-    data_files = ['eng']
-    if subject in data_files:
-        json_path = os.path.join('static', 'json', f'{subject}.json')
-        with open(json_path) as file:
-            file_contents = file.read()
-            parsed_json = json.loads(file_contents)
-            return parsed_json
-
-def question_answer_model(question,context):
-    qa_model_name = "deepset/roberta-base-squad2"
-    # a) Load model & tokenizer
-    model = AutoModelForQuestionAnswering.from_pretrained(qa_model_name)
-    tokenizer = AutoTokenizer.from_pretrained(qa_model_name)
-    nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
-    QA_input = {'question':question,'context':context}
-    return nlp(QA_input)['answer']
-    
-
-""" Open AI 
-def question_answer_model(question,context):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=f"Question: {question}\nContext: {context}\nAnswer:",
-        temperature=0.5,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        timeout=10
-    )
-
-    # Extract the answer from the API response
-    answer = response.choices[0].text.strip()
-    return answer
-"""
-def paraphrase(
-    question,
-    num_beams=5,
-    num_beam_groups=5,
-    num_return_sequences=5,
-    repetition_penalty=10.0,
-    diversity_penalty=3.0,
-    no_repeat_ngram_size=2,
-    temperature=0.7,
-    max_length=1000
-):
-    paraphrase_tokenizer = AutoTokenizer.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base")
-    paraphrase_model = AutoModelForSeq2SeqLM.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base").to(device)
-    input_ids = paraphrase_tokenizer(
-        f'paraphrase: {question}',
-        return_tensors="pt", padding="longest",
-        max_length=max_length,
-        truncation=True,
-    ).input_ids
-    
-    outputs = paraphrase_model.generate(
-        input_ids.to('cpu'), temperature=temperature, repetition_penalty=repetition_penalty,
-        num_return_sequences=num_return_sequences, no_repeat_ngram_size=no_repeat_ngram_size,
-        num_beams=num_beams, num_beam_groups=num_beam_groups,
-        max_length=max_length, diversity_penalty=diversity_penalty
-    )
-
-    res = paraphrase_tokenizer.batch_decode(outputs, skip_special_tokens=True)
-
-    return res
-
 
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def home():
     return render_template('index.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-
-
-@app.route("/contact")
-def contact():
-    return render_template('contact.html')
-
 
 @app.route("/question_answer")
 def question_answer():
@@ -106,7 +22,7 @@ def question_answer():
         else:
             print(question)
             print(context)
-            answer = question_answer_model(question,context)
+            answer = AI_models.question_answering(question,context)
             return render_template('question_answer.html',question=question,context=context,answer=answer)
 
     
@@ -118,7 +34,7 @@ def summarize():
             return render_template('summarize.html')
         else:
             print(query.replace("%0D%0A", "\n"))
-            summary_list=summarizer_model(query,max_length=len(query))
+            summary_list=AI_models.summarizer(query,max_length=len(query))
         return render_template('summarize.html',query=query,summary_list=summary_list)
 
 @app.route("/rephrase")
@@ -129,7 +45,7 @@ def rephrase():
             return render_template('rephrase.html')
         else:
             print(query.replace("%0D%0A", "\n"))
-            paraphrases = paraphrase(query,max_length=len(query))
+            paraphrases = AI_models.paraphrase(query,max_length=len(query))
         return render_template('rephrase.html',query=query,paraphrases=paraphrases)
 
 @app.route("/translate",methods=["GET"])
@@ -151,7 +67,7 @@ def question_analyzer():
             return render_template('question_analyzer.html')
         else:
             print(subject)
-            data = question_analyzer_model(subject)
+            data = AI_models.question_analyzer(subject)
             print(data)
             return render_template('question_analyzer.html',subject=subject,data=data)
 
